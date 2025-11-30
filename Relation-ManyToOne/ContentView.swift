b00483259d477300e4b1e23e8465d32b18e79788
Terje Moe <terjemoe54@hotmail.com>
2025-11-30 08:13:57 +0100

    Initial Commit

---
+++ Relation-ManyToOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Relation-ManyToOne
+//
+//  Created by Terje Moe on 30/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Relation-ManyToOne/Relation_ManyToOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Relation_ManyToOneApp.swift
+//  Relation-ManyToOne
+//
+//  Created by Terje Moe on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Relation_ManyToOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
